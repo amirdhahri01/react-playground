b82560a19a4533bad06f4d1706f09033aac451d6
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-04 20:51:31 +0100

    An update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import ReactDOM from 'react-dom/client';
-
+import { createElement } from 'react';
+import "./index.css"
+const MyAwesomeNavBar = () => {
+  return (
+     <div className="nav">
+      <ul className='nav-list'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Info</li>
+        <li>Testimonials</li>
+      </ul>
+     </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-   <ul>
-    <li>Js</li>
-    <li>Css</li>
-    <li>html</li>
-    <li>jquery</li>
-    <li>bootstrap</li>
-   </ul>
-);
+
+const reactElement  = <h1>Hello There</h1>
+console.log(MyAwesomeNavBar());
+
+root.render(MyAwesomeNavBar());
 
